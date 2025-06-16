047fc770843358957942c84d55a909ee474fd02a
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-16 14:06:46 +0000

    ex0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,33 +1,27 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * betty style doc for function therei
- * 
- * la
+ * main - entry point
+ *
+ * Return: 0 (if success)
  */
 int main(void)
 {
 	int n;
-
-	srand(time(0));
+		srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
-	{
-
-		printf("%d is positive\n", n);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
-	else 
+	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-
+	else
+	{
+		printf("%d is positive\n", n);
+	}
 	return (0);
 }
-
