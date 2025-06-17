1ccd175bce74f35bf50b90d23774f9b4d952cb94
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-17 12:08:22 +0000

    ex9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche toutes les combinaisons possibles de nombres à un chiffre
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+	int chiffre;
+
+	for (chiffre = 0; chiffre <= 9; chiffre++)
+	{
+		putchar(chiffre + '0'); /* Affichage du chiffre */
+		if (chiffre != 9)       /* Ajout de ",, " sauf après le dernier chiffre */
+		{
+			putchar(',');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n'); /* Ajout du saut de ligne */
+
+	return (0);
+}
