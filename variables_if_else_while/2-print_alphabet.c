a9f7e9697657a6e80040dc26e0d4e5cda0e6b706
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-17 08:46:36 +0000

    ex2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche l'alphabet en minuscules suivi d'un saut de ligne
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+    char lettre;
+
+    lettre = 'a';
+    while (lettre <= 'z')
+    {
+        putchar(lettre);
+        lettre++;
+    }
+    putchar('\n');
+
+    return (0);
+}
