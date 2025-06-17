1fc0f8e2871933df3f003889b7ed245916437eba
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-17 09:11:10 +0000

    ex3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche l'alphabet en minuscules et majuscules suivi d'un saut de ligne
+ *
+ * Return: Toujours 0 (succès)
+ */
+int main(void)
+{
+	char lettre;
+
+	/* Affichage des lettres minuscules */
+	for (lettre = 'a'; lettre <= 'z'; lettre++)
+		putchar(lettre);
+
+	/* Affichage des lettres majuscules */
+	for (lettre = 'A'; lettre <= 'Z'; lettre++)
+		putchar(lettre);
+
+	/* Ajout du saut de ligne */
+	putchar('\n');
+
+	return (0);
+}
