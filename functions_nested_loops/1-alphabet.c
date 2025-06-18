9ceed173c00cf4dc74aa53ba831e179c94a9f5ca
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-18 15:46:42 +0200

    ex1

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * main - Prints the lowercase alphabet using _putchar
- *
- * Return: Always 0
+ * print_alphabet - Prints the lowercase alphabet followed by a new line.
  */
+
 void print_alphabet(void)
 {
 	char lettre;
