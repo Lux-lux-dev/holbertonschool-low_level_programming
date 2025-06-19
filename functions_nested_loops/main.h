3439f49030b1f87ad8184f5acd8266a7c478d349
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-19 13:22:31 +0200

    ex3

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,30 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet(void); /* <- This line is crucial! */
+/**
+ * print_alphabet - Affiche l'alphabet en minuscule.
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - Affiche l'alphabet 10 fois.
+ */
 void print_alphabet_x10(void);
 
-int _putchar(char c); /* If you use _putchar */
+/**
+ * _islower - Vérifie si un caractère est une lettre minuscule.
+ * @c: Le caractère à vérifier.
+ *
+ * Return: 1 si c est une minuscule, 0 sinon.
+ */
+int _islower(int c);
+
+/**
+ * _putchar - Écrit un caractère dans stdout.
+ * @c: Le caractère à afficher.
+ *
+ * Return: Le caractère écrit.
+ */
+int _putchar(char c);
 
 #endif /* MAIN_H */
