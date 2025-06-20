27f567402e4227d5adf6b52acac01d94620050b0
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-20 11:15:05 +0200

    ex11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - Affiche tous les entiers de n à 98
+ * @n: Point de départ
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
+}
