099b3f96294ae3333518ccd7868517d701c62624
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-23 13:59:36 +0200

    ex5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints digits 0–14 * 10.
+ */
+void more_numbers(void)
+{
+	char c;
+	char d;
+
+	for (c = 0; c <= 10; c++)
+	{
+		for (d = 0; d <= 14; d++)
+		{
+			if (d >= 10)
+				_putchar('1');
+			_putchar('0' + (d % 10));
+
+		}
+		_putchar('\n');
+	}
+}
