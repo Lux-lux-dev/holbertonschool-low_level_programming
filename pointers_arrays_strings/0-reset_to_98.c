28408e5ffbaa0fe11b891e2de23eacbe695ba86e
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-24 12:11:18 +0200

    ex0

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+ * reset_to_98 - MAJ de la valeur pointée par le pointeur vers 98.
+ * @n: Pointeur vers un entier à modifier.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
