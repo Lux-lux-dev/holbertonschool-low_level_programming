b690e21c03b38a9a9d2a7933202eb25cae49a741
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-27 00:03:46 +0200

    ex

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,25 @@
-#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+#define SUCCESS 0
 
 /**
- * puts2 - Prints every other character of a string
- * @str: Input string
+ * puts2 - Print 1 char out of 2 of a string
+ * @str: The string
  */
 void puts2(char *str)
 {
 	int i = 0;
 
+	if (!str)
+		return;
+
 	while (str[i] != '\0')
 	{
 		if (i % 2 == 0)
-			putchar(str[i]);
+			write(1, &str[i], 1);
 		i++;
 	}
-	putchar('\n');
+	write(1, "\n", 1);
 }
