874c78c7559aaf7189177432bd794d56f44df7eb
Lux-lux-dev <lleblondytio@gmail.com>
2025-06-26 23:59:29 +0200

    ex6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * puts2 - Prints every other character of a string
+ * @str: Input string
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+		i++;
+	}
+	putchar('\n');
+}
